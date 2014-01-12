56aa07675fa99e43fde7085204b6ecf4a73cb9cd
Burak Sarac <bsarac@arrow.ca>
2014-01-12 10:23:07 -0800

    Expression Evaluator refactored

--- expression_evaluator.js
+++ expression_evaluator.js
@@ -1,46 +1,193 @@
-var exp_eval_options = {
+var exp_eval_constants = {
 	OPERATOR : "operator",
 	OPERAND : "operand",
+	INVALID_SYMBOL : "invalid_symbol",
 	SEGMENT_START : "segment_start",
 	SEGMENT_END : "segment_end",
-
-	SEGMENT_START_CHAR : "(",
-	SEGMENT_END_CHAR : ")",
-
 	VARIABLE : "variable",
 	CONSTANT : "constant",
+	BINARY : "binary",
+	UNARY : "unary",
+}
 
-	VARIABLE_PREFIX : "_",
+function ExpEvalOptions() {
+	
+	this.SYMBOL_PATTERN = "";
 
-	SYMBOL_PATTERN : "(\\|\\||&&|==|!=|<=|<|>=|>|\\(|\\)|\\w+|-?\\d+|\\+|-|\\*|/|%)",
+	this.SEGMENT_START_CHAR = "(";
+	this.SEGMENT_START_CHAR_REGEX = "\\(";
 
-	OPERATORS : {
-		"||" : 1,
+	this.SEGMENT_END_CHAR = ")";
+	this.SEGMENT_END_CHAR_REGEX = "\\)";
 
-		"&&" : 2,
+	this.VARIABLE_PREFIX = "_";
+	this.VARIABLE_REGEX = "_\\w+";
 
-		"==" : 3,
-		"!=" : 3,
+	this.CONSTANT_REGEX = "-?\\d+";
 
-		"<"  : 4,
-		">"  : 4,
-		"<=" : 4,
-		">=" : 4,
+	this.OPERATORS = {
 
-		"+"  : 5,
-		"-"  : 5,
+		"||" : {
+			precedence : 1,
+			regex_symbol : '\\|\\|',
+			type : exp_eval_constants.BINARY,
+			perform : function(o1,o2) {
+				return o1 || o2 ? true : false;
+			}
+		},
 
-		"*"  : 6,
-		"/"  : 6,
-		"%"  : 6
-	},
-};
+		"&&" : {
+			precedence : 2,
+			regex_symbol : '&&',
+			type : exp_eval_constants.BINARY,
+			perform : function(o1,o2) {
+				return o1 && o2 ? true : false;
+			}
+		},
 
-function ExpEval(expression, dataSource) {
+		"==" : {
+			precedence : 3,
+			regex_symbol : '==',
+			type : exp_eval_constants.BINARY,
+			perform : function(o1,o2) {
+				return o1 == o2 ? true : false;
+			}
+		},
 
-	this.__dataSource = dataSource;
-	this.__symbolList = new Array();
-	this.__variableList = new Array();
+		"!=" : {
+			precedence : 3,
+			regex_symbol : '!=',
+			type : exp_eval_constants.BINARY,
+			perform : function(o1,o2) {
+				return o1 != o2 ? true : false;
+			}
+		},
+
+		"<=" : {
+			precedence : 4,
+			regex_symbol : '<=',
+			type : exp_eval_constants.BINARY,
+			perform : function(o1,o2) {
+				return o1 <= o2 ? true : false;
+			}
+		},
+
+		"<" : {
+			precedence : 4,
+			regex_symbol : '<',
+			type : exp_eval_constants.BINARY,
+			perform : function(o1,o2) {
+				return o1 < o2 ? true : false;
+			}
+		},
+
+		">=" : {
+			precedence : 4,
+			regex_symbol : '>=',
+			type : exp_eval_constants.BINARY,
+			perform : function(o1,o2) {
+				return o1 >= o2 ? true : false;
+			}
+		},
+
+		">" : {
+			precedence : 4,
+			regex_symbol : '>',
+			type : exp_eval_constants.BINARY,
+			perform : function(o1,o2) {
+				return o1 > o2 ? true : false;
+			}
+		},
+
+		"+"  : {
+			precedence : 5,
+			regex_symbol : '\\+',
+			type : exp_eval_constants.BINARY,
+			perform : function(o1,o2) {
+				return parseFloat(o1) + parseFloat(o2);
+			}
+		},
+
+		"-"  : {
+			precedence : 5,
+			regex_symbol : '-',
+			type : exp_eval_constants.BINARY,
+			perform : function(o1,o2) {
+				return parseFloat(o1) - parseFloat(o2);
+			}
+		},
+
+		"*"  : {
+			precedence : 6,
+			regex_symbol : '\\*',
+			type : exp_eval_constants.BINARY,
+			perform : function(o1,o2) {
+				return parseFloat(o1) * parseFloat(o2);
+			}
+		},
+
+		"/"  : {
+			precedence : 6,
+			regex_symbol : '/',
+			type : exp_eval_constants.BINARY,
+			perform : function(o1,o2) {
+				return parseFloat(o1) / parseFloat(o2);
+			}
+		},
+
+		"%"  : {
+			precedence : 6,
+			regex_symbol : '%',
+			type : exp_eval_constants.BINARY,
+			perform : function(o1,o2) {
+				return parseFloat(o1) % parseFloat(o2);
+			}
+		},
+
+		"^"  : {
+			precedence : 7,
+			regex_symbol : '\\^',
+			type : exp_eval_constants.BINARY,
+			perform : function(o1,o2) {
+				return Math.pow(parseFloat(o1) , parseFloat(o2));
+			}
+		}
+	};
+
+	this.__buildSymbolPattern();
+}
+
+ExpEvalOptions.prototype.__buildSymbolPattern = function()
+{
+	this.SYMBOL_PATTERN = "(";
+
+	for(var key in this.OPERATORS)
+	{
+		var operator = this.OPERATORS[key];
+
+		this.SYMBOL_PATTERN += operator.regex_symbol + "|";
+	}
+
+	this.SYMBOL_PATTERN += this.SEGMENT_START_CHAR_REGEX + "|";
+	this.SYMBOL_PATTERN += this.SEGMENT_END_CHAR_REGEX + "|";
+	this.SYMBOL_PATTERN += this.VARIABLE_REGEX + "|";
+	this.SYMBOL_PATTERN += this.CONSTANT_REGEX;
+
+	this.SYMBOL_PATTERN += ")";
+}
+
+var default_exp_eval_options = new ExpEvalOptions();
+
+function ExpEval(expression, data_source, exp_eval_options) {
+
+	if (exp_eval_options == null)
+		this.options = default_exp_eval_options;
+	else
+		this.options = exp_eval_options;
+
+	this.__data_source = data_source;
+	this.__symbol_list = new Array();
+	this.__variable_list = new Array();
 
 	this.__fillSymbolList(expression);
 	this.__fillVariableList();
@@ -48,77 +195,77 @@ function ExpEval(expression, dataSource) {
 
 ExpEval.prototype.__fillSymbolList = function(expression)
 {
-	var symbol_list = expression.split(new RegExp(exp_eval_options.SYMBOL_PATTERN));
+	var symbol_list = expression.split(new RegExp(this.options.SYMBOL_PATTERN));
 	for (var key in symbol_list)
 	{
 		var symbol = symbol_list[key];
-		if (this.__getSymbolType(symbol) != exp_eval_options.INVALID_SYMBOL)
-			this.__symbolList.push(symbol);
+		if (this.__getSymbolType(symbol) != exp_eval_constants.INVALID_SYMBOL)
+			this.__symbol_list.push(symbol);
 	}
 }
 
 ExpEval.prototype.__getSymbolType = function(symbol)
 {
 	if (symbol.length == 0)
-		return exp_eval_options.INVALID_SYMBOL;
+		return exp_eval_constants.INVALID_SYMBOL;
 
-	if ( null != exp_eval_options.OPERATORS[symbol] )
-		return exp_eval_options.OPERATOR;
+	if ( null != this.options.OPERATORS[symbol] )
+		return exp_eval_constants.OPERATOR;
 
-	if (symbol == exp_eval_options.SEGMENT_START_CHAR)
-		return exp_eval_options.SEGMENT_START;
+	if (symbol == this.options.SEGMENT_START_CHAR)
+		return exp_eval_constants.SEGMENT_START;
 
-	if (symbol == exp_eval_options.SEGMENT_END_CHAR)
-		return exp_eval_options.SEGMENT_END;
+	if (symbol == this.options.SEGMENT_END_CHAR)
+		return exp_eval_constants.SEGMENT_END;
 
-	return exp_eval_options.OPERAND;
+	return exp_eval_constants.OPERAND;
 }
 
 ExpEval.prototype.__getOperandType = function(operand)
 {
 	if (typeof operand !== 'string')
-		return exp_eval_options.CONSTANT;
+		return exp_eval_constants.CONSTANT;
 
-	if (operand.search(exp_eval_options.VARIABLE_PREFIX) == 0)
-		return exp_eval_options.VARIABLE;
+	if (operand.search(this.options.VARIABLE_PREFIX) == 0)
+		return exp_eval_constants.VARIABLE;
 
-	return exp_eval_options.CONSTANT;
+	return exp_eval_constants.CONSTANT;
 }
 
 ExpEval.prototype.__getVariableName = function(operand)
 {
-	return operand.slice(exp_eval_options.VARIABLE_PREFIX.length);
+	return operand.slice(this.options.VARIABLE_PREFIX.length);
 }
 
 ExpEval.prototype.__fillVariableList = function()
 {
-	for(var key in this.__symbolList)
+	for(var key in this.__symbol_list)
 	{
-		var symbol = this.__symbolList[key];
+		var symbol = this.__symbol_list[key];
 		
 		var symbol_type = this.__getSymbolType(symbol);
-		if (symbol_type == exp_eval_options.OPERAND)
+		if (symbol_type == exp_eval_constants.OPERAND)
 		{
 			var operand_type = this.__getOperandType(symbol);
-			if (operand_type == exp_eval_options.VARIABLE)
-				this.__variableList.push(this.__getVariableName(symbol));
+			if (operand_type == exp_eval_constants.VARIABLE)
+				this.__variable_list.push(this.__getVariableName(symbol));
 		}
 	}
 }
 
 ExpEval.prototype.__getOperatorPresendence = function(operator)
 {
-	return exp_eval_options.OPERATORS[operator];
+	return this.options.OPERATORS[operator].precedence;
 }
 
 ExpEval.prototype.__getVariableValue = function(operand)
 {
 	var res;
 
-	if ( this.__getOperandType(operand) == exp_eval_options.VARIABLE )
+	if ( this.__getOperandType(operand) == exp_eval_constants.VARIABLE )
 	{
 		var variable_name = this.__getVariableName(operand);
-		res = this.__dataSource.onNeedValue(variable_name);
+		res = this.__data_source.onNeedValue(variable_name);
 	}
 	else
 		res = operand;
@@ -130,31 +277,9 @@ ExpEval.prototype.__operateOn = function (operand1, operand2, operator)
 {
 	var o1 = this.__getVariableValue(operand1);
 	var o2 = this.__getVariableValue(operand2);
-	var res = false;
+	var op = this.options.OPERATORS[operator];
 
-	switch(operator)
-	{
-		case "||" : res = o1 || o2 ? true : false; break;
-
-		case "&&" : res = o1 && o2 ? true : false; break;
-
-		case "==" : res = o1 == o2 ? true : false; break;
-		case "!=" : res = o1 != o2 ? true : false; break;
-
-		case "<"  : res = o1 <  o2 ? true : false; break;
-		case ">"  : res = o1 >  o2 ? true : false; break;
-		case "<=" : res = o1 <= o2 ? true : false; break;
-		case ">=" : res = o1 >= o2 ? true : false; break;
-
-		case "+"  : res = parseFloat(o1) + parseFloat(o2); break;
-		case "-"  : res = parseFloat(o1) - parseFloat(o2); break;
-
-		case "*"  : res = parseFloat(o1) * parseFloat(o2); break;
-		case "/"  : res = parseFloat(o1) / parseFloat(o2); break;
-		case "%"  : res = parseFloat(o1) % parseFloat(o2); break;
-	}
-
-	return res;
+	return op.perform(o1, o2);
 }
 
 ExpEval.prototype.evaluate = function ()
@@ -162,40 +287,41 @@ ExpEval.prototype.evaluate = function ()
 	var operatorList = new Array();
 	var operandList = new Array();
 
-	for(var key in this.__symbolList)
+	for(var key in this.__symbol_list)
 	{
-		var symbol = this.__symbolList[key];
+		var symbol = this.__symbol_list[key];
 
 		var type = this.__getSymbolType(symbol);
 
 		switch(type)
 		{
-			case exp_eval_options.OPERATOR:
+			case exp_eval_constants.OPERATOR:
 				if (operatorList.length > 0)
 				{
 					var top_operator = operatorList[operatorList.length-1];
 
-					if ( this.__getOperatorPresendence(symbol) < this.__getOperatorPresendence(top_operator) )
-					{
-						operatorList.pop();
-						var operand2 = operandList.pop();
-						var operand1 = operandList.pop();
+					if (top_operator != this.options.SEGMENT_START_CHAR)
+						if ( this.__getOperatorPresendence(symbol) < this.__getOperatorPresendence(top_operator) )
+						{
+							operatorList.pop();
+							var operand2 = operandList.pop();
+							var operand1 = operandList.pop();
 
-						var result = this.__operateOn(operand1, operand2, top_operator);
-						operandList.push(result);
-					}
+							var result = this.__operateOn(operand1, operand2, top_operator);
+							operandList.push(result);
+						}
 				}
 
 				operatorList.push(symbol);
 			break;
 
-			case exp_eval_options.SEGMENT_END:
+			case exp_eval_constants.SEGMENT_END:
 				var top_operator;
 				do
 				{
 					top_operator = operatorList.pop();
 
-					if (top_operator != exp_eval_options.SEGMENT_START_CHAR)
+					if (top_operator != this.options.SEGMENT_START_CHAR)
 					{
 						var operand2 = operandList.pop();
 						var operand1 = operandList.pop();
@@ -203,14 +329,14 @@ ExpEval.prototype.evaluate = function ()
 						var result = this.__operateOn(operand1, operand2, top_operator);
 						operandList.push(result);
 					}							
-				} while (top_operator != exp_eval_options.SEGMENT_START_CHAR && operatorList.length > 0)
+				} while (top_operator != this.options.SEGMENT_START_CHAR && operatorList.length > 0)
 				break;
 
-			case exp_eval_options.SEGMENT_START:
+			case exp_eval_constants.SEGMENT_START:
 				operatorList.push(symbol)
 				break;
 
-			case exp_eval_options.OPERAND:
+			case exp_eval_constants.OPERAND:
 				operandList.push(symbol);
 				break;
 		}
@@ -231,5 +357,5 @@ ExpEval.prototype.evaluate = function ()
 
 ExpEval.prototype.getVariableList = function()
 {
-	return this.__variableList;
+	return this.__variable_list;
 }
